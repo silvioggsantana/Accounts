078a01e4cd5b9572eec50ef0c0255604c5e1941a
Silvio Gabriel <silvioggsantana@gmail.com>
2025-10-29 16:08:10 -0300

    adicionando função operation

--- index.js
+++ index.js
@@ -2,4 +2,28 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 import fs from 'fs';
 
-console.log('projeto rodando')
+operation()
+
+function operation(){
+
+    inquirer.prompt([
+        {
+            type:'list',
+            name:'action',
+            message:'o que você deseja fazer?',
+            choices: [
+                'Criar conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair',
+            ]
+        }
+    ])
+    .then((answer => {
+        console.log(`você escolheu o ${answer.action}`)
+    }))
+    .catch((e) => {
+        console.log(e)
+    })
+}
