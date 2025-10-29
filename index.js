bb4afa8a804c51f6855489ee58bb594029a6ed50
Silvio Gabriel <silvioggsantana@gmail.com>
2025-10-29 16:39:32 -0300

    adicionando criação de conta e opção de sair

--- index.js
+++ index.js
@@ -21,7 +21,63 @@ function operation(){
         }
     ])
     .then((answer => {
-        console.log(`você escolheu o ${answer.action}`)
+
+        if(answer.action === 'Criar conta'){
+            crateAccount()
+        } else if(answer.action === 'Consultar Saldo'){
+
+        } else if(answer.action === 'Depositar'){
+
+        } else if(answer.action === 'Sacar'){
+
+        } else if(answer.action === 'Sair'){
+            process.exit()
+        }
+    }))
+    .catch((e) => {
+        console.log(e)
+    })
+}
+
+function crateAccount(){
+    console.log(chalk.bgGreen.black("Obrigado pela preferência de escolher o nosso banco!"))
+    console.log("defina as opções da sua conta a seguir")
+
+    buildAccount();
+}
+
+function buildAccount(){
+        inquirer.prompt([
+        {
+
+            name:'action',
+            message:'digite um nome para sua conta?',
+        }
+    ])
+    .then((answer => {
+
+        console.log(`O nome da sua conta é ${answer.action}`)
+
+        if(!fs.existsSync(`accounts`)){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${answer.action}.json`)){
+            console.log(chalk.bgRed.black('Erro: Já existe uma conta com esse nome'))
+            buildAccount()
+            return;
+        }
+
+        fs.writeFileSync(
+            `accounts/${answer.action}.json`,
+            '{"balance" : 0}',
+            function(e){
+                console.log(e)
+            }
+        )
+
+        console.log(`Parabéns sua conta foi criada!`)
+        operation()
     }))
     .catch((e) => {
         console.log(e)
